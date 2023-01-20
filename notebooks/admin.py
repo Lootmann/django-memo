51a317e98520859c851d40213d169aaa2775df83
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-20 09:29:35 +0900

    notebooks: impl admin.py

--- notebooks/admin.py
+++ notebooks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from notebooks.models import NoteBook
+
+
+class NoteBookAdmin(admin.ModelAdmin):
+    model = NoteBook
+    list_display = ["title", "content"]
+
+
+admin.site.register(NoteBook, NoteBookAdmin)
