bd4baa80f82edf85209240787a3bc35d12b40e3c
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-21 08:50:42 +0900

    notebook: impl search form

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from pages.views import PagesIndexView
+from pages.views import PagesIndexView, PagesSearchView
 
 app_name = "pages"
 
 urlpatterns = [
     path("", PagesIndexView.as_view(), name="index"),
+    path("search/", PagesSearchView.as_view(), name="search"),
 ]
